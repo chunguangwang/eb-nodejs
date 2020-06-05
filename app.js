463385f77e811fc078bfa4225437c28cda4298d9
Chunguang Wang <chunguangwang@Chunguangs-MacBook-Pro.local>
2020-06-05 15:16:41 -0700

    adding response body

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var log = function(entry) {
 
 var server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
+        console.log("hello post request");
         var body = '';
 
         req.on('data', function(chunk) {
@@ -17,11 +18,12 @@ var server = http.createServer(function (req, res) {
 
         req.on('end', function() {
             if (req.url === '/') {
-                log('Received message: ' + body);
+                console.log('Received message: ' + body);
             } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+                console.log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
-
+            res.write(body);
+            res.end();
             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
             res.end();
         });
