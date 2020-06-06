ce8b5f4a0ccc555253ed553a396656216759e8a1
Chunguang Wang <chunguangwang@Chunguangs-MacBook-Pro.local>
2020-06-05 21:08:47 -0700

    usig react built

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
 
 const productRoutes = require('./api/products');
 
